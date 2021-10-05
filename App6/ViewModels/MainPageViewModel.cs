9889feea2fc860e90e4c572148a8350f70252fff
David-barce <71525685+David-barce@users.noreply.github.com>
2021-10-04 23:00:56 -0500

    Agregar archivos de proyecto.

---
+++ App6/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,72 @@
+﻿using App6.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+
+namespace App6.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        Command _goToViewHomeCommand;
+        string username;
+        string password;
+        private bool isError;
+
+        public MainPageViewModel(INavigation navigation) : base(navigation)
+        {
+
+        }
+
+        public string Username
+        {
+            get => username;
+            set
+            {
+
+                username = value;
+                OnPropertyChanged();
+
+            }
+        }
+
+        public string Password
+        {
+            get => password;
+            set
+            {
+                password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command GoToViewHomeCommand
+        {
+            get => _goToViewHomeCommand ?? (_goToViewHomeCommand = new Command(GoToViewHomeAction));
+        }
+
+        public bool IsError
+        {
+            get => isError;
+            set 
+            {
+                isError = value;
+                OnPropertyChanged();
+            
+            }
+
+        }
+
+        private void GoToViewHomeAction()
+        {
+            IsError = !(Username == "David" && Password == "123456");
+            if (!IsError)
+            {
+                Navigation.PushAsync(new PageHome());
+            }
+
+
+        }
+    }
+}
---
+++ App6/ViewModels/PageHomeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App6.ViewModels
+{
+    public class PageHomeViewModel : BaseViewModel
+    {
+        Command _goToRoot;
+        public PageHomeViewModel(INavigation navigation) : base(navigation)
+        {
+
+        }
+
+        public Command GoToRootCommand
+        {
+            get => _goToRoot ?? (_goToRoot = new Command(GoToRootAction));
+        }
+
+        private void GoToRootAction()
+        {
+            Navigation.PopToRootAsync();
+        }
+    }
+}
